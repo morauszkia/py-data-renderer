c353b1c3b891339a9798d61e26b521b393670b44
mantis <morauszki.andras@gmail.com>
2026-01-30 19:58:29 +0100

    Feat(data): Load_table function to load csv and excel files

--- core/data_loader.py
+++ core/data_loader.py
@@ -1,8 +1,9 @@
 import pandas as pd
+from core.util import get_filepath_data
 from pathlib import Path
 
 
-def load_table(file_path: Path) -> None:
+def load_table(project_dir: Path) -> pd.DataFrame:
     """
     Loads CSV or XLSX and returns a list of dicts.
 
@@ -15,13 +16,16 @@ def load_table(file_path: Path) -> None:
     Returns:
         List[Dict[str, Any]]: _description_
     """
+    file_path = get_filepath_data(project_dir)
     if file_path.suffix == ".csv":
         print("Loading CSV file...")
         df = pd.read_csv(file_path, encoding="utf-8")
+        print(f"File: {file_path} loaded successfully")
         return df
     elif file_path.suffix in [".xls", ".xlsx"]:
         print("Loading Excel file...")
         df = pd.read_excel(file_path)
+        print(f"File: {file_path} loaded successfully")
         return df
     else:
         raise ValueError("Unsupported file format")
